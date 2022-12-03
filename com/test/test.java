322d8af5acb945c3ca948cd6e2ae2f8511970f17
aoidayo <aoidayo@163.com>
2022-12-03 20:26:29 +0800

    First commit

--- com/test/test.java
+++ com/test/test.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class test {
+package com.test;
+
+/**
+ * 测试git上传
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");//111
+        //222
+        //333
+        //测试更新后的git上传
+    }
 }
